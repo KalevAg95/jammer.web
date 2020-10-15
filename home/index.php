eb3ce3599065d2f65235825c284ab064e6d3e578
Kalev Aguirre <kalevag95@gmail.com>
2020-10-15 18:22:32 -0500

    initial commit

---
+++ home/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./stylesheets/homepage.css">
+        <title>¡Una página con estilo!</title>
+    </head>
+    <body>
+    <!-- Site navigation menu -->
+        <ul class="navbar">
+            <li><a href="index.html">Inicio</a>
+            <li><a href="musings.html">Animales Marinos</a>
+            <li><a href="town.html">Animales Terrestres</a>
+            <li><a href="links.html">Animales Voladores</a>
+        </ul>
+        <!-- Main content -->
+        <div class="container">
+            <h1>¡Esta es una página con mucho estilo!</h1>
+            <h2>Bienvenidos a mi página de animales con estilo.</h2>
+            <p>Aún no tiene imágenes pero si tiene estilo. También tiene ligas, aunque no lleven a ningún lado</p>
+            <p>Pronto tendrá más contenido, ¡así que tengan paciencia!</p>
+        </div>
+        <footer>Hecha con amor el <?php echo date("Y/m/d");?>, con mucho amor.</footer>
+    </body>
+ </html>
